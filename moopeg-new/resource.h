582f6c7426649b529f6fd05af76ba79f33ab8821
Laurent Raufaste <analogue@glop.org>
2018-03-17 16:40:37 -0700

    Upload legacy moopeg source code

---
+++ moopeg-new/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by playwnd.rc
+//
+#define IDI_MOOPEG                      102
+#define IDB_ABOUT                       103
+#define ID_Menu                         104
+#define ID_VIEW_FULLSCREEN              105
+#define ID_VIEW_ZOOM                    106
+#define ID_VIEW_ALWAYSONTOP             107
+#define ID_PLAY_PLAY                    108
+#define ID_PLAY_STOP                    109
+#define ID_PLAY_VOLUME                  110
+#define ID_ZOOM_50                      111
+#define ID_VOLUME_MUTE                  112
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        114
+#define _APS_NEXT_COMMAND_VALUE         40003
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
